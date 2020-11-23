ec3668c501da9b788bba76af86660a9660aa49b0
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2020-11-23 15:20:15 +0700

    reactive composition api

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,50 @@
 <template>
-    <h2>{{ count }}</h2>
+    <h2>{{ state.count }}, doubled = {{ state.doubled }}</h2>
     <button @click="updateCount(true)">Increase</button>
     <button @click="updateCount(false)">Decrease</button>
 </template>
 
 <script>
-import { ref } from "vue";
+// REF
+// import { ref } from "vue";
+
+// export default {
+//     setup() {
+//         const count = ref(0)
+
+//         function updateCount(increase) {
+//             if (increase)
+//                 count.value++
+//             else
+//                 count.value--
+//         }
+
+//         return {
+//             count,
+//             updateCount
+//         }
+//     }
+// }
+
+// REACTIVE
+import { reactive, computed } from "vue";
 
 export default {
     setup() {
-        const count = ref(0)
+        const state = reactive({
+            count: 0,
+            doubled: computed(() => state.count * 2)
+        })
 
         function updateCount(increase) {
             if (increase)
-                count.value++
+                state.count++
             else
-                count.value--
+                state.count--
         }
 
         return {
-            count,
+            state,
             updateCount
         }
     }
