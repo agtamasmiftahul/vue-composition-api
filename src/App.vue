75f7bc5853e2e5038f930f36c65066347d891923
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2020-11-23 15:04:59 +0700

    ref composition api

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>{{ count }}</h2>
+    <button @click="updateCount(true)">Increase</button>
+    <button @click="updateCount(false)">Decrease</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    setup() {
+        const count = ref(0)
+
+        function updateCount(increase) {
+            if (increase)
+                count.value++
+            else
+                count.value--
+        }
+
+        return {
+            count,
+            updateCount
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
